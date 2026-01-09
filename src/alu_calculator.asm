32aa8f748e01d39a7619de717f34dea5c6fdb037
Mudassar Khan <m.mudassaar.khan@gmail.com>
2026-01-09 10:08:37 +0500

    added divison function and handling zero divsion error

--- src/alu_calculator.asm
+++ src/alu_calculator.asm
@@ -6,17 +6,18 @@ extern ExitProcess
 global main
 
 section .data
-    menu   db 10,"1.Add  2.Sub  3.Mul  8.Exit",10,"Choice: ",0
-    fmtInt db "%d",0
-    fmtRes db "Result = %d",10,0
+    menu     db 10,"1.Add  2.Sub  3.Mul  4.Div  8.Exit",10,"Choice: ",0
+    fmtInt   db "%d",0
+    fmtRes   db "Result = %d",10,0
+    divErr   db "You can't divide by zero",10,0
 
     choice dd 0
-    A dd 0
-    B dd 0
+    A      dd 0
+    B      dd 0
 
 section .text
 main:
-    sub rsp, 40              ; shadow space ONCE
+    sub rsp, 40              ; shadow space (Windows x64)
 
 LOOP:
     ; show menu
@@ -51,6 +52,8 @@ LOOP:
     je DO_SUB
     cmp dword [choice], 3
     je DO_MUL
+    cmp dword [choice], 4
+    je DO_DIV
 
     jmp LOOP                 ; invalid choice → restart
 
@@ -65,6 +68,19 @@ DO_SUB:
 DO_MUL:
     imul eax, ebx
     jmp PRINT
+
+DO_DIV:
+    cmp ebx, 0
+    je DIV_ZERO
+
+    xor edx, edx             ; clear remainder
+    idiv ebx                 ; eax = eax / ebx
+    jmp PRINT
+
+DIV_ZERO:
+    lea rcx, [divErr]
+    call printf
+    jmp LOOP
 
 PRINT:
     lea rcx, [fmtRes]
